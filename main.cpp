0097d2cecf7745ce8a965f83f212eb856d02a504
Raymond Portilla <134645362+rmandors@users.noreply.github.com>
2024-11-30 22:00:44 -0500

    Add features for display

--- main.cpp
+++ main.cpp
@@ -18,16 +18,16 @@ Raymond Portilla - 00335050
 using namespace std;
 
 void displayMenu();
-void loadMapDataFromCSVfile(map<string, unique_ptr<Package>>&);
-void displayMapData(map<string, unique_ptr<Package>>&);
-void saveMapDataToBinaryFile(map<string, unique_ptr<Package>>&);
-void loadMapDataFromBinaryFile(map<string, unique_ptr<Package>>&);
-void displayPackagesByType(map<string, unique_ptr<Package>>&);
-void displayPackagesByCost(map<string, unique_ptr<Package>>&);
-void extractAndModifyPackage(map<string, unique_ptr<Package>>&);
+void loadMapDataFromCSVfile(map<string, Package*>&);
+void displayMapData(map<string, Package*>&);
+void saveMapDataToBinaryFile(map<string, Package*>&);
+void loadMapDataFromBinaryFile(map<string, Package*>&);
+void displayPackagesByType(map<string, Package*>&);
+void displayPackagesByCost(map<string, Package*>&);
+void extractAndModifyPackage(map<string, Package*>&);
 
 int main(){
-	map<string, unique_ptr<Package>> packages;
+	map<string, Package*> packages;
 	char menuChoice;
 
 	do{
@@ -50,6 +50,9 @@ int main(){
 				break;
 			
 			case '4':
+				for (auto& [id, package] : packages){
+    				delete package;
+				}
 				packages.clear();
 				cout << "The map has been emptied." << endl << endl;
 				break;	
@@ -97,7 +100,7 @@ void displayMenu(){
 		 << "[9] Exit." << endl << endl;
 }
 
-void loadMapDataFromCSVfile(map<string, unique_ptr<Package>>& packages){
+void loadMapDataFromCSVfile(map<string, Package*>& packages){
 	if(packages.empty()){
 		ifstream file("initialData.csv", ios::in);
 		if(!file){
@@ -112,8 +115,8 @@ void loadMapDataFromCSVfile(map<string, unique_ptr<Package>>& packages){
         	stringstream ss(row);
 
         	string id, name_sender, name_recipient, address_sender, address_recipient, city_sender, 
-		       city_recipient, state_sender, state_recipient, zipCode_senderStr, zipCode_recipientStr, 
-			   weightStr, costPerOnceStr, flatFeeForTwoDaysStr, overnightFeePerOnceStr;
+		           city_recipient, state_sender, state_recipient, zipCode_senderStr, zipCode_recipientStr, 
+			       weightStr, costPerOnceStr, flatFeeForTwoDaysStr, overnightFeePerOnceStr;
 
         	getline(ss, id, ',');
         	getline(ss, name_sender, ',');
@@ -139,17 +142,17 @@ void loadMapDataFromCSVfile(map<string, unique_ptr<Package>>& packages){
 			double overnightFeePerOnce = stod(overnightFeePerOnceStr);
 
 			if(flatFeeForTwoDays == 0 && overnightFeePerOnce == 0)
-        		packages[id] = make_unique<Package>(id, name_sender, name_recipient, address_sender, address_recipient, 
-												city_sender, city_recipient, state_sender, state_recipient, zipCode_sender, 
-												zipCode_recipient,weight, costPerOnce);
+        		packages[id] = new Package(id, name_sender, name_recipient, address_sender, address_recipient, 
+										   city_sender, city_recipient, state_sender, state_recipient, zipCode_sender, 
+										   zipCode_recipient,weight, costPerOnce);
 			else if(overnightFeePerOnce == 0)
-				packages[id] = make_unique<TwoDayPackage>(id, name_sender, name_recipient, address_sender, address_recipient, 
-			                   						  city_sender, city_recipient, state_sender, state_recipient, zipCode_sender, 
-													  zipCode_recipient, weight, costPerOnce, flatFeeForTwoDays);
+				packages[id] = new TwoDayPackage(id, name_sender, name_recipient, address_sender, address_recipient, 
+			                   					 city_sender, city_recipient, state_sender, state_recipient, zipCode_sender, 
+												 zipCode_recipient, weight, costPerOnce, flatFeeForTwoDays);
 			else
-		    	packages[id] = make_unique<OvernightPackage>(id, name_sender, name_recipient, address_sender, address_recipient, 
-													     city_sender, city_recipient, state_sender, state_recipient, zipCode_sender, 
-													     zipCode_recipient, weight, costPerOnce, overnightFeePerOnce);
+		    	packages[id] = new OvernightPackage(id, name_sender, name_recipient, address_sender, address_recipient, 
+												   city_sender, city_recipient, state_sender, state_recipient, zipCode_sender, 
+												   zipCode_recipient, weight, costPerOnce, overnightFeePerOnce);
     	}
 	
 		cout << "Data has been correctly loaded!" << endl << endl;
@@ -160,32 +163,32 @@ void loadMapDataFromCSVfile(map<string, unique_ptr<Package>>& packages){
 	}
 } 
 
-void displayMapData(map<string, unique_ptr<Package>>& packages){
+void displayMapData(map<string, Package*>& packages){
 	if(!packages.empty()){
     	double totalShippingCost = 0;
 	
 		for (const auto& [id, package] : packages) {
-    		cout << *package << endl;
+    		cout << endl << *package;
 			totalShippingCost += package->calculateCost();
     	}
 
-		cout << "Total Shipping Cost: " << totalShippingCost << endl << endl;
+		cout << endl << "Total Shipping Cost: " << totalShippingCost << endl << endl;
 	}
 	else{
 		cout << "The map is empty." <<endl << endl;
 	}
 }
 
-void saveMapDataToBinaryFile(map<string, unique_ptr<Package>>& packages){
-	if(packages.empty()){
+void saveMapDataToBinaryFile(map<string, Package*>& packages){
+	if(!packages.empty()){
 
 	}
 	else{
-		cout << "The map already contains information. Empty the map, then try again!" << endl << endl;
+		cout << "The map is empty." << endl << endl;
 	}	
 }
 
-void loadMapDataFromBinaryFile(map<string, unique_ptr<Package>>& packages){
+void loadMapDataFromBinaryFile(map<string, Package*>& packages){
 	if(packages.empty()){
 
 	}
@@ -194,33 +197,42 @@ void loadMapDataFromBinaryFile(map<string, unique_ptr<Package>>& packages){
 	}
 }
 
-void displayPackagesByType(map<string, unique_ptr<Package>>& packages){
-	if(!packages.empty()){
-    	
-	
-	}
-	else{
-		cout << "The map is empty." <<endl << endl;
-	}
-}
-
-void displayPackagesByCost(map<string, unique_ptr<Package>>& packages){
+void displayPackagesByType(map<string, Package*>& packages){
     if(!packages.empty()){
-   		vector<unique_ptr<Package>> sortedPackages;
-        vector<string> keys;  
+        vector<Package*> packageType;
+        vector<TwoDayPackage*> twoDayPackageType;
+        vector<OvernightPackage*> overnightPackageType;
 
-        for (auto& [id, package] : packages) {
-            keys.push_back(id); 
-            sortedPackages.push_back(make_unique<Package>(*package));
+        for(const auto& [id, package] : packages){
+            if(auto p = dynamic_cast<TwoDayPackage*>(package))
+                twoDayPackageType.push_back(p); 
+            else if(auto p = dynamic_cast<OvernightPackage*>(package))
+                overnightPackageType.push_back(p); 
+			else if(auto p = dynamic_cast<Package*>(package))
+                packageType.push_back(p);
         }
 
-        sort(sortedPackages.begin(), sortedPackages.end(), [](const unique_ptr<Package>& a, const unique_ptr<Package>& b){
-            return a->calculateCost() > b->calculateCost();
-        });
-
-        for (auto& package : sortedPackages) {
-            cout << *package << endl; 
+        if(!packageType.empty()){
+            cout << "\n.:Packages of Type 'Package':." << endl << endl;
+            for(const auto& package : packageType){
+                cout << *package << endl;
+            }
         }
+
+        if(!twoDayPackageType.empty()){
+            cout << "\n.:Packages of Type 'TwoDayPackage':." << endl << endl;
+            for(const auto& package : twoDayPackageType){
+                cout << *package << endl;
+            }
+        }
+
+        if(!overnightPackageType.empty()){
+            cout << "\n.:Packages of Type 'OvernightPackage':." << endl << endl;
+            for(const auto& package : overnightPackageType){
+                cout << *package << endl;
+            }
+        }
+
         cout << endl;
     }
 	else{
@@ -228,6 +240,38 @@ void displayPackagesByCost(map<string, unique_ptr<Package>>& packages){
 	}
 }
 
-void extractAndModifyPackage(map<string, unique_ptr<Package>>& packages){
+void displayPackagesByCost(map<string, Package*>& packages){
+   if (!packages.empty()) {
+        vector<Package*> sortedPackages;
 
+        for (auto& [id, package] : packages){
+            sortedPackages.push_back(new Package(*package)); 
+        }
+
+        sort(sortedPackages.begin(), sortedPackages.end(), [](const Package* a, const Package* b){
+            return a->calculateCost() > b->calculateCost();
+        });
+
+        for (auto& package : sortedPackages) {
+            cout << endl << *package;
+            delete package;  
+        }
+
+        cout << endl;;
+    }
+	else{
+		cout << "The map is empty." <<endl << endl;
+	}
+}
+
+void extractAndModifyPackage(map<string, Package*>& packages){
+	int num, user_id;
+	cout << "Enter the number of packages you want modify: ";
+	cin >> num;
+	
+	for(int i=0; i<num; i++){
+
+		cout << "Enter the id of the package you want to modify: ";
+		cin >> user_id;
+	}
 }
